194fcc330ce888ece2e0cc0241dd26c64cbdabd8
Felix Bork <fbork91@icloud.com>
2014-01-01 16:51:04 +0100

    Created a src folder for C++ classes in Eclipse and added a main.cpp
    In this commit, the Eclipse Git_Test project was modified and a main.cpp
    class was added.

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+
+    if(2 == 2)
+    {
+        std::cout << "LoL" << std::endl;
+    }
+
+    bool condition = true;
+    if(condition)
+    {
+        std::cout<<"The constant PI has the value" <<M_PI <<std::endl;
+    }
+}
